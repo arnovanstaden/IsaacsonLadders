d5b5ad6f62cb740726df46d28099aed4351c1a37
arnovanstaden <arnovanstaden@gmail.com>
2019-02-06 10:44:51 +0200

    finish contact page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
+// Hide Navbar on scroll
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
 // Smooth Scrolling
 
 $(".products-arrow").click(function() {
     document.querySelector('.landing-products-section').scrollIntoView({ behavior: 'smooth' });
 });
+
